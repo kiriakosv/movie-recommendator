bb0a7d021f4d3e0a4354a965c793177628d9fbaf
Kiriakos Velissariou <kir.velissariou@gmail.com>
2015-04-07 18:32:10 +0300

    Changes to admin page

--- moviesite/moviesite/settings.py
+++ moviesite/moviesite/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_DIRS= [os.path.join(BASE_DIR, 'templates')]
+
 ROOT_URLCONF = 'moviesite.urls'
 
 WSGI_APPLICATION = 'moviesite.wsgi.application'

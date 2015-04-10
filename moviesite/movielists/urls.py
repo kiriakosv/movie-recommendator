515fbd65117571685e4d3bf862551810219903ed
Kiriakos Velissariou <kir.velissariou@gmail.com>
2015-04-10 17:31:33 +0300

    Added movies by genre page

--- moviesite/movielists/urls.py
+++ moviesite/movielists/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.index, name = 'index'),
+        url(r'^(?P<genre>\D+)/$', views.movies_by_genre,
+            name='movies_by_genre'),
 ]
--- moviesite/movielists/views.py
+++ moviesite/movielists/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from .models import Movie
+
 def index(request):
     context = {}
     return render(request, 'movielists/index.html', context)
 
+def movies_by_genre(request, genre):
+    movies_of_selected_genre = Movie.objects.all().filter(genre=genre)
+    context = {'movies_of_selected_genre': movies_of_selected_genre}
+    return render(request, 'movielists/movies_by_genre.html', context)

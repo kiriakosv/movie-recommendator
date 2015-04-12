88dc9bf5eb017fa5ee1b488aab9f8a04ea7b6feb
Kiriakos Velissariou <kir.velissariou@gmail.com>
2015-04-12 17:46:35 +0300

    Put static files in mainp

--- moviesite/moviesite/urls.py
+++ moviesite/moviesite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
@@ -17,3 +18,4 @@ urlpatterns += patterns('',
             'document_root': settings.MEDIA_ROOT,
         }),
 )
+

34bfdc65fa14aa898b9a45ba5b551d8a3977932e
Kiriakos Velissariou <kir.velissariou@gmail.com>
2015-04-16 22:56:29 +0300

    Added tests to recommender app

--- moviesite/recommender/tests.py
+++ moviesite/recommender/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from .models import RecommenderQuestion, RecommenderChoice
+
+class RecommenderTests(TestCase):
+    def test_questions_appear(self):
+        """
+        If questions exist, display them
+        """
+        response = self.client.get(reverse('recommender:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "age")

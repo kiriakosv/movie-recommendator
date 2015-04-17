3245659f597c644e901a62b40018e6c2f764418e
Kiriakos Velissariou <kir.velissariou@gmail.com>
2015-04-17 18:43:31 +0300

    Started recommendation algorithm

--- moviesite/recommender/views.py
+++ moviesite/recommender/views.py
@@ -10,5 +10,22 @@ def index(request):
     return render(request, 'recommender/index.html', context)
 
 def results(request):
-    context = {}
+    counter = {"CR": 0, "TH": 0, "SF": 0, "DR": 0, "CM": 0, "AD": 0}
+
+    post_keys = request.POST.keys()
+    for a_key in post_keys:
+        trunct = a_key[0:2]
+        if trunct == "cs":
+            continue
+        if trunct != "LE" and trunct != "AG":
+            value = int(request.POST[a_key])
+            counter[trunct] += value
+
+    a = []
+    for a_key in request.POST.keys():
+        a_key = a_key[0:2]
+        if a_key not in ['cs', 'LE', 'AG']:
+            a.append(counter[a_key])
+    context = {'a': a}
+
     return render(request, 'recommender/results.html', context)
